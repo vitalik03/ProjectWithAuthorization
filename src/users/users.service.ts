a3921f4e20672b4c8dd154bf0377e747669a8d1d
Vitalik <vitalik.vovryk@gmail.com>
2019-08-19 22:23:07 +0300

    Add methods: create, update, delete for users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { IUser } from './interfaces/user.interface';
+import { InjectModel } from '@nestjs/mongoose';
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel('User') readonly userModel){}
+    
+    async addUser(user: IUser){
+        const new_user = this.userModel(user);
+        return await new_user.save();
+    }
+
+    async deleteUser(name: string){
+        const params = {
+            name
+        }
+        await this.userModel.findOneAndDelete(params);
+        return "success";
+    }
+
+    async updateUser(user: IUser)
+    {
+        const name = user.username;
+        const params = {
+            name
+        }
+        const email = user.password;
+        const aparams = {
+            email
+        }
+        const updated = await this.userModel.findOneAndUpdate(params,aparams,{new: true});
+        return updated; 
+    }
+}
