a8982de1303d402752029bb96310f1dedb960517
Vitalik <vitalik.vovryk@gmail.com>
2019-08-20 12:54:13 +0300

    Add auth guards to messages controllers

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post, Body, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Put, UseGuards } from '@nestjs/common';
 import { IMessage } from './interface/message.interface';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('messages')
 export class MessagesController {
     constructor(private readonly messageService: MessagesService){}
  
+    @UseGuards(AuthGuard('jwt'))
     @Post()
     addUser(@Body() createMessage: CreateMessageDto): Promise<IMessage> {
       return this.messageService.addMessage(createMessage);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,26 @@
-import { Controller, Get, Post, Body, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Put, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { IUser } from './interfaces/user.interface';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService){}
+    constructor( private readonly usersService: UsersService ) {}
     
+    @UseGuards( AuthGuard( 'jwt' ) )
     @Post()
-    addUser(@Body() createUser: CreateUserDto): Promise<IUser> {
+    addUser( @Body() createUser: CreateUserDto ): Promise<IUser> {
       return this.usersService.addUser(createUser);
     } 
 
     @Delete()
-      deleteUser(@Body() createUser: CreateUserDto){
+      deleteUser( @Body() createUser: CreateUserDto ){
         return this.usersService.deleteUser(createUser.username);
       }
 
     @Put()
-      updateUser(@Body() user: CreateUserDto): Promise<IUser>{
+      updateUser( @Body() user: CreateUserDto ): Promise<IUser> {
         return this.usersService.updateUser(user);
       }
 }
